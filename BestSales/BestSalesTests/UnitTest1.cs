9bf76e94d7a626ab197c2ba314e6023eba1a9179
robson081192 <robson081192@gmail.com>
2015-04-18 21:06:32 +0200

    bez generatora

---
+++ BestSales/BestSalesTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BestSales.Models.DbInsert;
+using BestSales;
+namespace BestSalesTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            RandomCarGenerator generator = new RandomCarGenerator();
+
+            generator.InsertRandomCar();
+        }
+    }
+}
