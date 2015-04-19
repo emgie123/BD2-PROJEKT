7fe75bb58279a4e7001587e082d9a42de7900be4
robson081192 <robson081192@gmail.com>
2015-04-20 00:00:02 +0200

    Działająca zakładka Contact (AJAX)

--- BestSales/BestSales/Controllers/HomeController.cs
+++ BestSales/BestSales/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 using BestSales.Models.DbInsert;
 using BestSales.Models.DB;
+using BestSales.Models.UserInfo;
 
 
 namespace BestSales.Controllers
@@ -46,6 +48,25 @@ namespace BestSales.Controllers
             return View();
         }
 
+        public ActionResult ShowUser(string userid)
+        {
+            User user = null;
+            if (int.Parse(userid) == 1)
+            {
+                user = new User("mg.png", "Garbacz Michał", "Junior Web Developer");
+                user.SetDetails("DEVELOPERS", "987654321", "majkel@BestSales.com", new List<string>() { "Programowanie", "Bazy Danych" }, new List<string>() { "ASP.NET MVC, JavaScript, SQL" });
+            }
+            else if (int.Parse(userid) == 2)
+            {
+                user = new User("robson.jpeg", "Winkler Robert", "Junior Web Developer");
+                user.SetDetails("DEVELOPERS", "123456789", "robson@BestSales.com", new List<string>() {"Technologie internetowe", "Programowanie"}, new List<string>() { "ASP.NET MVC, JavaScript, HTML, CSS" });
+            }
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("~/Views/Partials/ContactPartial.cshtml", user);
+            }
+            return View("Contact", user);
+        }
 
        
     }
---
+++ BestSales/BestSales/Models/UserInfo/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace BestSales.Models.UserInfo
+{
+    public class User
+    {
+        public string ThumbnailName { get; set; }
+        public Dictionary<string, string> Details { get; set; }
+        public string Name { get; set; }
+        public string Profession { get; set; }
+
+        public User(string thumbnail, string name, string profession)
+        {
+            ThumbnailName = thumbnail;
+            Name = name;
+            Profession = profession;
+
+            Details = new Dictionary<string, string>();
+        }
+
+        public void SetDetails(string department, string phone, string email, List<string> hobbysList,
+            List<string> skillsList)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Details.Add("DZIAŁ:", department);
+            Details.Add("TELEFON:", phone);
+            Details.Add("EMAIL:", email);
+
+            foreach (var hobby in hobbysList)
+            {
+                sb.Append(hobby + "\n");
+            }
+            Details.Add("ZAINTERESOWANIA:", sb.ToString());
+
+            sb.Clear();
+            foreach (var skill in skillsList)
+            {
+                sb.Append(skill + "\n");
+            }
+            Details.Add("UMIEJĘTNOŚCI:", sb.ToString());
+
+        }
+    }
+}
